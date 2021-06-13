377d120eb3e6083025e4bdf0f5b23b70bc04ff55
ghdwpaks <hjm7904@gmail.com>
2021-06-14 06:03:55 +0900

    naver4 문제 진도 및 readme.txt를 통해 과정 추가 기록

--- naver4/naver4/src/naver4/pro2.java
+++ naver4/naver4/src/naver4/pro2.java
@@ -9,26 +9,16 @@ public class pro2 {
 		try {
 			showTokens(s, ", ");
 		} catch (NoSuchElementException e) {
-			// TODO: handle exception
 			System.out.println("³¡");
 		}
 
 	}
 	public static void showTokens(String s,String keys) {
 		int j = -1;
-		//System.out.println(s.length());
 		while (true) {
 			j++;
-			//System.out.println("{ j = "+j);
 			boolean canpass = true;
 			for (int i = 0; i < keys.length(); i++) {
-				/*
-				System.out.println();
-				System.out.println("s.charAt("+j+") = "+s.charAt(j));
-				System.out.println("keys.charAt("+i+") = "+keys.charAt(i));
-				System.out.println("s.charAt("+j+") == keys.charAt("+i+") : "+(s.charAt(j) == keys.charAt(i)));
-				System.out.println();
-				*/
 				if(s.charAt(j) == keys.charAt(i)) {
 					System.out.println("");
 					canpass = false;
@@ -37,13 +27,10 @@ public class pro2 {
 			}
 			if (canpass) {
 				System.out.print(s.charAt(j));
-				//System.out.print("prints = '"+s.charAt(j)+"'");
-				//System.out.println("\n}");
 			}
 			if (s.length()-1 == j) {
 				break;
 			}
-			//System.out.print("j = "+j);
 		}
 	}
 }
