d2daf1bb99f07f0ac7ea64caf17c75adf39774ca
ghdwpaks <hjm7904@gmail.com>
2021-06-14 06:26:16 +0900

    naver4 temp commit

--- naver4/naver4/src/naver4/pro1.java
+++ naver4/naver4/src/naver4/pro1.java
@@ -1,10 +1,20 @@
 package naver4;
 
+
+import java.util.*;
+
 public class pro1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Max<Number> n = new Max<>();
+		System.out.println(n.Max());
+		
+		
 	}
-
+	public static class Max<t> {
+		public <T> T Max(Object obj1,Object obj2) {
+			
+			return ();
+		}
+	}
 }
